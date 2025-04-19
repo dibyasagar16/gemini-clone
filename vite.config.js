f703739495627193d484536e61c7f7d6b096994f
dibyasagar16 <samaldibyasagar@gmail.com>
2025-04-19 16:10:20 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/gemini-clone",
+  base: "/gemini-clone/", // 👈 Add this subpath
+  build: {
+    assetsDir: "assets",
+    emptyOutDir: true,
+  },
 });
