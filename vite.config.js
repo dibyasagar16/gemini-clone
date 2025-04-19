d4b35289ed3dea4913e81a037ed3472debfb0428
dibyasagar16 <samaldibyasagar@gmail.com>
2025-04-19 16:21:19 +0530

    Revert "Updated vite.config.js"
    This reverts commit f703739495627193d484536e61c7f7d6b096994f.
    
    Reverting vite.conffig.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gemini-clone/", // 👈 Add this subpath
-  build: {
-    assetsDir: "assets",
-    emptyOutDir: true,
-  },
+  plugins: [react()],
+  base: "/gemini-clone",
 });
